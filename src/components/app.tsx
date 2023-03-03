0f72f52fe16030c3303fac39995995cc6f54c7f4
Doug Martin <dmartin@concord.org>
2023-03-03 16:30:04 -0500

    Working integrated text output

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import {clsx} from "clsx";
 
 import { useCODAP } from "../hooks/use-codap";
@@ -9,47 +9,40 @@ import { Node } from "../type";
 
 import "./app.scss";
 
-type Destination = "text component" | "dataset";
-type GenerationMode = "ready" | "playing" | "paused" | "steping"
+type GenerationMode = "ready" | "playing" | "paused" | "steping";
 
 const AnyStartingState = "(any)";
 const MaxLengthLimit = 25;
 
+type SequenceGroup = {
+  startingState: string;
+  delimiter: string;
+  lengthLimit: number;
+  sequences: string[];
+};
+
 export const App = () => {
-  const [destination, setDestination] = useState<Destination>("text component");
   const [lengthLimit, setLengthLimit] = useState<number|undefined>(5);
   const [delimiter, setDelimiter] = useState("");
   const [startingState, setStartingState] = useState("");
-  const [textSequenceHeader, setTextSequenceHeader] = useState("");
+  const [sequenceGroups, setSequenceGroups] = useState<SequenceGroup[]>([]);
   const [animateNode, setAnimateNode] = useState<Node|undefined>(undefined);
   const [generationMode, setGenerationMode] = useState<GenerationMode>("ready");
+  const prevAnimatedSequenceGroups = useRef<SequenceGroup[]>([]);
+  const currentAnimatedSequenceGroup = useRef<SequenceGroup>();
 
   const {graph, updateGraph} = useGraph();
-  const {dragging, outputToDataset, outputTextSequence} = useCODAP({onCODAPDataChanged: updateGraph});
+  const {dragging, outputToDataset} = useCODAP({onCODAPDataChanged: updateGraph});
   const {generate} = useGenerator();
+  const innerOutputRef = useRef<HTMLDivElement|null>(null);
+
+  useEffect(() => {
+    if (innerOutputRef.current && sequenceGroups.length > 0) {
+      innerOutputRef.current.scrollTop = innerOutputRef.current.scrollHeight;
+    }
+  }, [sequenceGroups]);
 
   const graphEmpty = useCallback(() => graph.nodes.length === 0, [graph]);
-
-  const instructions = () => {
-    if (graphEmpty()) {
-      return (
-        <div className="instructions">
-          <p>
-            This plugin generates sequences of text using a Markov chain. The plugin uses a Markov chain built from a
-            dataset in CODAP. The dataset must have a column of states. The plugin will build a Markov chain from the
-            states, and then allow generation of a sequence of text using the Markov chain.
-          </p>
-          <p>
-            To use the plugin, first drag an attribute into the plugin.
-          </p>
-        </div>
-      );
-    }
-  };
-
-  const handleChangeDestination = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDestination(e.target.value as Destination);
-  };
 
   const handleChangeLengthLimit = (e: React.ChangeEvent<HTMLInputElement>) => {
     const numberValue = parseInt(e.target.value, 10);
@@ -64,15 +57,6 @@ export const App = () => {
     setStartingState(e.target.value);
   };
 
-  const generateSequenceHeader = () => {
-    const parts = [
-      `starting state: ${startingState.length > 0 ? startingState : AnyStartingState}`,
-      `delimiter: ${delimiter === "" ? "(none)" : `"${delimiter}"`}`,
-      `max length: ${lengthLimit}`
-    ];
-    return `---- ${parts.join(", ")} ----`;
-  };
-
   const handlePause = () => {
     // TODO
   };
@@ -81,9 +65,13 @@ export const App = () => {
     // TODO
   };
 
+  const handleClear = () => {
+    // TODO
+  };
+
   const handleGenerate = useCallback(async () => {
     if (lengthLimit !== undefined) {
-      let newTextSequenceHeader: string|undefined;
+      //let newTextSequenceHeader: string|undefined;
       const startingNode = startingState.length > 0 ? graph.nodes.find(n => n.id === startingState) : undefined;
 
       setGenerationMode("playing");
@@ -91,39 +79,38 @@ export const App = () => {
       const generatedResult = await generate(graph, {startingNode, lengthLimit});
       const sequence = generatedResult.map(n => n.label);
 
-      if (destination === "text component") {
-        newTextSequenceHeader = generateSequenceHeader();
-        if (newTextSequenceHeader === textSequenceHeader) {
-          newTextSequenceHeader = undefined;
-        }
+      prevAnimatedSequenceGroups.current = [...sequenceGroups];
+      currentAnimatedSequenceGroup.current =
+        prevAnimatedSequenceGroups.current[prevAnimatedSequenceGroups.current.length - 1];
+      if (!currentAnimatedSequenceGroup.current ||
+          (currentAnimatedSequenceGroup.current.delimiter !== delimiter) ||
+          (currentAnimatedSequenceGroup.current.lengthLimit !== lengthLimit) ||
+          (currentAnimatedSequenceGroup.current.startingState !== startingState)) {
+        currentAnimatedSequenceGroup.current = {delimiter, lengthLimit, startingState, sequences: []};
+        setSequenceGroups(prevAnimatedSequenceGroups.current);
+      } else {
+        prevAnimatedSequenceGroups.current.pop();
       }
 
-      await outputTextSequence("append", "", newTextSequenceHeader);
+      const prevSequences = [...currentAnimatedSequenceGroup.current.sequences];
+      currentAnimatedSequenceGroup.current.sequences.push("");
 
       await animateNodes(generatedResult, async (animatedNodes) => {
-        await outputTextSequence(
-          "replace",
-          animatedNodes.map(node => node.label).join(delimiter),
-          newTextSequenceHeader
-        );
-
+        if (currentAnimatedSequenceGroup.current) {
+          const animatedSequence = animatedNodes.map(node => node.label).join(delimiter);
+          currentAnimatedSequenceGroup.current.sequences = [...prevSequences, animatedSequence];
+          setSequenceGroups([...prevAnimatedSequenceGroups.current, currentAnimatedSequenceGroup.current]);
+        }
       });
 
       setGenerationMode("ready");
 
-      switch (destination) {
-        case "text component":
-          if (newTextSequenceHeader) {
-            setTextSequenceHeader(newTextSequenceHeader);
-          }
-          await outputTextSequence("replace", sequence.join(delimiter), newTextSequenceHeader);
-          break;
-        case "dataset":
-          await outputToDataset(sequence);
-          break;
-      }
+      currentAnimatedSequenceGroup.current.sequences = [...prevSequences, sequence.join(delimiter)];
+      setSequenceGroups([...prevAnimatedSequenceGroups.current, currentAnimatedSequenceGroup.current]);
+
+      await outputToDataset(sequence);
     }
-  }, [delimiter, destination, generate, generateSequenceHeader, graph, lengthLimit, outputTextSequence, outputToDataset, startingState, textSequenceHeader]);
+  }, [generate, graph, lengthLimit, outputToDataset, startingState, sequenceGroups, delimiter]);
 
   const animateNodes = (nodes: Node[], callback: (animatedNodes: Node[]) => void) => {
     // eslint-disable-next-line no-async-promise-executor
@@ -152,48 +139,36 @@ export const App = () => {
     if (!graphEmpty()) {
       return (
         <div className="generate">
-          <div>
-            <div>Where to put generated states</div>
+          <div className="flex-col">
             <div>
-              <input
-                type="radio"
-                name="destinationInput"
-                value={"text component"}
-                checked={destination === "text component"}
-                onChange={handleChangeDestination} /> Text Component
-              <input
-                type="radio"
-                name="destinationInput"
-                value={"dataset"}
-                checked={destination === "dataset"}
-                onChange={handleChangeDestination} /> Dataset
+              <label>Starting State:</label>
+              <select onChange={handleChangeStartingState} value={startingState}>
+                <option value="">{AnyStartingState}</option>
+                {graph.nodes.map(n => <option key={n.id} value={n.id}>{n.id}</option>)}
+              </select>
             </div>
-          </div>
-          <div className="flex-row">
-          <label> Starting State:
-            <select onChange={handleChangeStartingState} value={startingState}>
-              <option value="">{AnyStartingState}</option>
-              {graph.nodes.map(n => <option key={n.id} value={n.id}>{n.id}</option>)}
-            </select>
-          </label>
-          <label> Max Length:
-            <input type="number"
-                   value={lengthLimit}
-                   onChange={handleChangeLengthLimit}
-                   min={1}
-                   max={MaxLengthLimit}
-                   style={{width: "50px"}}
-            />
-          </label>
-          {destination === "text component" &&
-            <label>Delimiter:
-              <input type="text"
-                     onChange={handleChangeDelimiter}
-                     value={delimiter}
-                     maxLength={3}
-                     style={{width: "20px"}}
-              />
-            </label>}
+
+            <div className="flex-row">
+              <div>
+                <label>Max Length:</label>
+                <input type="number"
+                      value={lengthLimit}
+                      onChange={handleChangeLengthLimit}
+                      min={1}
+                      max={MaxLengthLimit}
+                />
+              </div>
+
+              <div>
+                <label>Delimiter:</label>
+                <input type="text"
+                        onChange={handleChangeDelimiter}
+                        value={delimiter}
+                        placeholder="(none)"
+                        maxLength={3}
+                />
+              </div>
+            </div>
           </div>
           <div className="buttons">
             <button
@@ -214,12 +189,71 @@ export const App = () => {
     }
   };
 
+  const sequenceOutput = () => {
+    return (
+      <div className="sequence-output">
+        <div className="output">
+          <div className="inner-output" ref={innerOutputRef}>
+            {sequenceGroups.map((group, i) => {
+              return (
+                <div className="group" key={i}>
+                  <div className="header">
+                    <span>{group.startingState.length > 0 ? group.startingState : AnyStartingState}</span>
+                    <span>/</span>
+                    <span>{group.lengthLimit}</span>
+                    <span>/</span>
+                    <span>{group.delimiter === "" ? "(none)" : `"${group.delimiter}"`}</span>
+                  </div>
+                  <div className="sequences">
+                    {group.sequences.map((s, j) => <div key={j}>{s}</div>)}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="buttons">
+          <button
+            type="button"
+            onClick={handleClear}
+            disabled={lengthLimit === undefined}>
+              Clear Output
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  if (graphEmpty()) {
+    return (
+      <div className={clsx("app", {dragging})}>
+        <div className="instructions">
+          <h2>Markov Chains</h2>
+          <p>
+            This plugin generates sequences of text using a Markov chain. The plugin uses a Markov chain built from a
+            dataset in CODAP. The dataset must have a column of states. The plugin will build a Markov chain from the
+            states, and then allow generation of a sequence of text using the Markov chain.
+          </p>
+          <p>
+            To use the plugin, first drag an attribute into the plugin.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={clsx("app", {dragging})}>
-      <h2>Markov Chains</h2>
-      {instructions()}
-      {uiForGenerate()}
-      <Graph graph={graph} animateNode={animateNode} />
+      <div className="split">
+        <div className="left">
+          <h2>Markov Chains</h2>
+          <Graph graph={graph} animateNode={animateNode} />
+        </div>
+        <div className="right">
+          {uiForGenerate()}
+          {sequenceOutput()}
+        </div>
+      </div>
     </div>
   );
 };
--- src/hooks/use-codap.ts
+++ src/hooks/use-codap.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   getValuesForAttribute,
   // entityInfo,
@@ -12,10 +12,9 @@ import codapInterface from "../utils/codap-interface";
 const PluginName = "Markov Chain";
 const PluginVersion = "0.16";
 const InitialDimensions = {
-  width: 429,
+  width: 600,
   height: 420
 };
-const TextComponentName = "Sequences";
 const OutputDatasetName = "Sequence Data";
 
 export type CODAPAttribute = {
@@ -28,17 +27,11 @@ export type OnCODAPDataChanged = (values: string[]) => void;
 
 export type OutputTextMode = "replace"|"append";
 
-type Sequence = {
-  value?: string;
-  header?: string;
-};
-
 export const useCODAP = ({onCODAPDataChanged}: {onCODAPDataChanged: OnCODAPDataChanged}) => {
   const [initialized, setInitialized] = useState(false);
   const [dragging, setDragging] = useState(false);
   const [attribute, setAttribute] = useState<CODAPAttribute|undefined>(undefined);
   const [sequenceNumber, setSequenceNumber] = useState(0);
-  const generatedSequencesRef = useRef<Sequence[]>([]);
 
   const setPluginState = (values: any) => {
     // TODO: use values
@@ -91,45 +84,6 @@ export const useCODAP = ({onCODAPDataChanged}: {onCODAPDataChanged: OnCODAPDataC
   const incrementSequenceNumber = useCallback(() => {
     setSequenceNumber(prev => prev + 1);
   }, [setSequenceNumber]);
-
-  const guaranteeTextComponent = useCallback(async () => {
-    let tFoundValue: any = null;
-
-    // Verify text component exists
-    const tListResult: any = await codapInterface.sendRequest({
-      "action": "get",
-      "resource": "componentList"
-    }).catch((reason) => {
-      console.log("unable to get component list because " + reason);
-    });
-    if (tListResult?.success) {
-      tFoundValue = tListResult.values.find((iValue: any) => {
-        return iValue.type === "text" && iValue.name === TextComponentName;
-      });
-    }
-    if (!tFoundValue) {
-      // Create text component
-      const tCreateResult: any = await codapInterface.sendRequest({
-        "action": "create",
-        "resource": "component",
-        values: {
-          type: "text",
-          name: TextComponentName,
-          dimensions: {width: 200, height: 400},
-          position: "top"
-        }
-      }).catch((reason) => {
-        console.log("unable to create text component because " + reason);
-      });
-      if (!(tCreateResult?.success)) {
-        console.log("unable to create text component");
-        return;
-      }
-      return tCreateResult.values.id;
-    } else {
-      return tFoundValue.id;
-    }
-  }, []);
 
   const guaranteeOutputDatasetAndCaseTable = useCallback(async () => {
     let tFoundValue: any = null;
@@ -211,73 +165,6 @@ export const useCODAP = ({onCODAPDataChanged}: {onCODAPDataChanged: OnCODAPDataC
     }
   }, []);
 
-  const outputToTextComponent = useCallback(async (sequences: Sequence[]) => {
-    const textComponentID = await guaranteeTextComponent();
-
-    const children: any[] = [];
-    sequences.forEach((iSequence, index) => {
-      if (iSequence.header !== undefined) {
-        if (index > 0) {
-          children.push({
-            type: "paragraph",
-            children: [{text: ""}]
-          });
-        }
-        children.push({
-          type: "paragraph",
-          children: [{text: iSequence.header}]
-        });
-      }
-      if (iSequence.value !== undefined) {
-        children.push({
-          type: "paragraph",
-          children: [{text: iSequence.value}]
-        });
-      }
-    });
-    await codapInterface.sendRequest({
-      action: "update",
-      resource: `component[${textComponentID}]`,
-      values: {
-        text: {
-          "object": "value",
-          "document": {
-            children,
-            "objTypes": {
-              "paragraph": "block"
-            }
-          }
-        }
-      }
-    });
-  }, [guaranteeTextComponent]);
-
-  const outputTextSequence = useCallback(async (mode: OutputTextMode, sequence: string, header?: string) => {
-    let newGeneratedSequences: Sequence[];
-    let lastSequence: Sequence|undefined;
-
-    if (mode === "append") {
-      newGeneratedSequences = [...generatedSequencesRef.current, {value: sequence, header}];
-    } else {
-      lastSequence = generatedSequencesRef.current[generatedSequencesRef.current.length - 1];
-      if (lastSequence) {
-        lastSequence.value = sequence;
-        if (header) {
-          lastSequence.header = header;
-        }
-        newGeneratedSequences = [...generatedSequencesRef.current];
-      } else {
-        newGeneratedSequences = [{value: sequence, header}];
-      }
-    }
-    generatedSequencesRef.current = newGeneratedSequences;
-    await outputToTextComponent(newGeneratedSequences);
-  }, [outputToTextComponent]);
-
-  const outputTextHeader = useCallback(async (header: string) => {
-    await outputToTextComponent([...generatedSequencesRef.current, {value: header}]);
-  }, [outputToTextComponent]);
-
   const outputToDataset = useCallback(async (sequence: string[]) => {
     await guaranteeOutputDatasetAndCaseTable();
     const requests = sequence.map((iState, iIndex) => {
@@ -323,7 +210,5 @@ export const useCODAP = ({onCODAPDataChanged}: {onCODAPDataChanged: OnCODAPDataC
   return {
     dragging,
     outputToDataset,
-    outputTextSequence,
-    outputTextHeader
   };
 };
